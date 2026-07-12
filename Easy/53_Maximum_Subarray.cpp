f6308cc30d37c266f145100fba9e262c37d6f524
Sunny Raj <265703685+Sunnyraj-me@users.noreply.github.com>
2026-07-12 13:51:27 +0530

    Solved LeetCode 53 - Maximum Subarray

---
+++ Easy/53_Maximum_Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum=0;
+        int maxSum=INT_MIN;
+        for(int x:nums){
+            currSum+=x;
+            maxSum=max(currSum,maxSum);
+            if (currSum<0)
+            {
+                currSum = 0;
+            }
+            
+        }
+        return maxSum;
+        return -1;
+    }
+};
